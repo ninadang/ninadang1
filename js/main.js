ecaffbbefdaa99963d3f49c0a37bedd5a7cd93b9
Nina Dang <ninadangt@gmail.com>
2013-12-15 14:26:26 -0800

    jdkl;jadkls

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(function() {
+
+	var viewportheight = $(window).height();
+	$('.slide').height(viewportheight);
 	
 	//hide the all of the element with class msg_body
 	$(".msg-body").hide();
 	
 	//toggle the componenet with class msg_body
 	$(".msg-head").click(function(){
-		$(this).next(".msg-body").slideToggle(600);
+		//for each .msg-body
+		//if .attr('display') == "none" then
+			//hide me
+		//
+		var target = $(this).next(".msg-body");
+		target.slideToggle(600);
+		target.addClass('active');
+		
 	});
 });
