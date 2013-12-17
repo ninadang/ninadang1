138cf968e4f256fb4e135879f0c13e602036bee3
Nina Dang <ninadangt@gmail.com>
2013-12-16 21:53:02 -0800

    Updated screenshots and added slow js

--- js/main.js
+++ js/main.js
@@ -4,32 +4,17 @@ $(function() {
 
 	$('#slide1, #slide2, #slide3, #slide4, #slide5').height(viewportheight);
 
-/*
-	//hide the all of the element with class msg_body
-	$(".msg-body").hide();
-	
-	//toggle the componenet with class msg_body
-	$(".msg-head").click(function(){
-		//for each .msg-body
-		//if .attr('display') == "none" then
-			//hide me
-		//
-		$(".msg-body").hide();
-		var target = $(this).next(".msg-body");
-		target.slideToggle(600);
-		target.addClass('active');
-*/
 	$("#narr-pics").hide();
 	$("#card-pics").hide();
 
 	//toggle the componenet with class msg_body
 	$(".work-narr").click(function(){
 		$("#card-pics").hide();
-		$("#narr-pics").show();
+		$("#narr-pics").show("slow");
 	});
 
 	$(".work-card").click(function(){
 		$("#narr-pics").hide();
-		$("#card-pics").show();
+		$("#card-pics").show("slow");
 	});
 });
