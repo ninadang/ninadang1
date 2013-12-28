dbec8ca9ca86059a8041f9cb16ab70131eea31c3
Nina Dang <ninadangt@gmail.com>
2013-12-28 13:16:44 -0800

    added infocrafts and changed black of slides

--- js/main.js
+++ js/main.js
@@ -5,18 +5,20 @@ $(function() {
 	$('#slide1, #slide2, #slide3, #slide4, #slide5').height(viewportheight);
 
 	$("#narr-pics").hide();
-	//$("#card-pics").hide();
+	$("#info-pics").hide();
 	$("#quiet-pics").hide();
 	
 	$(".work-narr").click(function(){
 			$("#card-pics").hide();
 			$("#quiet-pics").hide();
+			$("#info-pics").hide();
 			$("#narr-pics").toggle("slow");
 	});
 
 	$(".work-card").click(function(){
 		$("#narr-pics").hide();
 		$("#quiet-pics").hide();
+		$("#info-pics").hide();
 		$("#card-pics").toggle("slow");
 	});
 
@@ -24,7 +26,15 @@ $(function() {
 	$(".work-quiet").click(function(){
 		$("#narr-pics").hide();
 		$("#card-pics").hide();
+		$("#info-pics").hide();
 		$("#quiet-pics").toggle("slow");
+	});
+
+	$(".work-info").click(function(){
+		$("#narr-pics").hide();
+		$("#card-pics").hide();
+		$("#quiet-pics").hide();
+		$("#info-pics").toggle("slow");
 	});
 
 	// img tag clicker
